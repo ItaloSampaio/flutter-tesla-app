7ba5f79a8c1f9e46eac04b23bd842961c419573a
Ítalo Sampaio <italo.arsampaio@gmail.com>
2021-10-27 22:15:00 -0300

    🔧: improve AssetPahts

--- lib/presentation/core/asset_paths.dart
+++ lib/presentation/core/asset_paths.dart
@@ -1,42 +1,30 @@
 class AssetPaths {
-  static final svgs = _SVGPaths._();
-  static final images = _ImagePaths._();
+  static const svgs = _SVGPaths._();
+  static const images = _ImagePaths._();
 
   AssetPaths._();
 }
 
 class _SVGPaths {
-  _SVGPaths._();
+  final battery = 'assets/svgs/battery.svg';
+  final car = 'assets/svgs/car.svg';
+  final carretUp = 'assets/svgs/carret_up.svg';
+  final charge = 'assets/svgs/charge.svg';
+  final coolShape = 'assets/svgs/cool_shape.svg';
+  final flTyre = 'assets/svgs/fl_tyre.svg';
+  final heatShape = 'assets/svgs/heat_shape.svg';
+  final lockRounded = 'assets/svgs/lock_rounded.svg';
+  final lock = 'assets/svgs/lock.svg';
+  final temp = 'assets/svgs/temp.svg';
+  final tyre = 'assets/svgs/tyre.svg';
+  final unlockRounded = 'assets/svgs/unlock_rounded.svg';
 
-  String get battery => 'assets/svgs/battery.svg';
-
-  String get car => 'assets/svgs/car.svg';
-
-  String get carretUp => 'assets/svgs/carret_up.svg';
-
-  String get charge => 'assets/svgs/charge.svg';
-
-  String get coolShape => 'assets/svgs/cool_shape.svg';
-
-  String get flTyre => 'assets/svgs/fl_tyre.svg';
-
-  String get heatShape => 'assets/svgs/heat_shape.svg';
-
-  String get lockRounded => 'assets/svgs/lock_rounded.svg';
-
-  String get lock => 'assets/svgs/lock.svg';
-
-  String get temp => 'assets/svgs/temp.svg';
-
-  String get tyre => 'assets/svgs/tyre.svg';
-
-  String get unlockRounded => 'assets/svgs/unlock_rounded.svg';
+  const _SVGPaths._();
 }
 
 class _ImagePaths {
-  _ImagePaths._();
+  final coolGlow = 'assets/images/cool_glow.png';
+  final hotGlow = 'assets/images/hot_glow.png';
 
-  String get coolGlow => 'assets/images/cool_glow.png';
-
-  String get hotGlow => 'assets/images/hot_glow.png';
+  const _ImagePaths._();
 }
